aa2ad565ba325ee0c827db3edf9f656533af68ea
Tim Hui <timboh56@gmail.com>
2016-03-08 03:30:47 -0800

    add some commenting and answering capabilities

--- app/controllers/question.js
+++ app/controllers/question.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  answer: {},
+  actions: {
+    submitAnswer() {
+      let answerHash =  this.get('answer');
+      let answerRecord = this.store.createRecord('answer', answerHash);
+      answerRecord.set('question', this.get('model'));
+      answerRecord.set('user', this.get('current-user.model'));
+      answerRecord.save();
+    },
+
+    resetAnswer() {
+      this.set('answer', {});
+    }
+  }
 });
--- app/mixins/commentable.js
+++ app/mixins/commentable.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-  comments: DS.hasMany('comment', { async: false, embedded: 'always' }),
+  comments: DS.hasMany('comment', { async: true, embedded: 'always' }),
   commentsCount: DS.attr('number')
 });
