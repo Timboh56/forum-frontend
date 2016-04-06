99587af7e1c2135d7b4706df2763280c724cb170
Tim Hui <timboh56@gmail.com>
2016-04-05 23:50:37 -0700

    deploy

--- config/deploy.js
+++ config/deploy.js
@@ -51,12 +51,12 @@ module.exports = function(deployTarget) {
     ENV.build.environment = 'production';
     ENV.s3.bucket = process.env.PRODUCTION_BUCKET;
     ENV.s3.region = process.env.PRODUCTION_REGION;
-    ENV.s3.hostName = process.env.PRODUCTION_HOST;
+    ENV.s3.host = process.env.PRODUCTION_HOST;
     ENV.cloudfront.distribution = process.env.PRODUCTION_DISTRIBUTION;
     ENV['s3-index'] = {
       bucket: process.env.PRODUCTION_BUCKET,
       region: 'us-west-1',
-      hostName: process.env.PRODUCTION_HOST
+      host: process.env.PRODUCTION_HOST
     };
 
   }
