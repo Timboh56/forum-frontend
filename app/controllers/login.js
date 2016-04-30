921f8fc5b2e08851f72c0d6eb05b815acf68eb0a
Tim Hui <timboh56@gmail.com>
2016-04-30 01:45:43 -0700

    adding functionality to login

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,4 +1,46 @@
 import Ember from 'ember';
+import ENV from '../config/environment';
 
 export default Ember.Controller.extend({
+  session: Ember.inject.service('session'),
+  options: {
+    email: '',
+    password: ''
+  },
+
+  actions: {
+    login() {
+      let options = this.get('options'),
+        Session = this.get('session');
+
+      return new Ember.RSVP.Promise((resolve, reject) => {
+          Ember.$.ajax({
+            url: ENV.APP.LOGIN_PATH,
+            type: 'POST',
+            data: JSON.stringify({
+                username: options.email,
+                password: options.password
+            }),
+            beforeSend(xhr) {
+              xhr.setRequestHeader('Accept', 'application/vnd.api+json');
+              xhr.setRequestHeader('AUTHORIZATION_TOKEN',Session.get('authToken'));
+              xhr.setRequestHeader('AUTHORIZATION', ENV.APIKEY);
+            },
+            contentType: 'application/json;charset=utf-8',
+            dataType: 'json'
+          }).then(function(response) {
+            Ember.run(function() {
+              resolve({
+                token: response.id_token
+              });
+            });
+          }, function(xhr, status, error) {
+              var response = xhr.responseText;
+              Ember.run(function() {
+                  reject(response);
+              });
+          });
+      });
+    }
+  }
 });
