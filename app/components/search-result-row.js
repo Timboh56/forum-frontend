b4c06cb6008c1acde649bdc9b3b44c22ef875ba2
Tim Hui <timboh56@gmail.com>
2016-05-01 03:06:11 -0700

    restyle user details on result row for search forums

--- app/components/search-result-row.js
+++ app/components/search-result-row.js
@@ -1,9 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-  actions: {
-    goToQuestion: function(id) {
-      this.sendAction('goToQuestion', id);
-    }
-  }
-});
+export default Ember.Component.extend();
