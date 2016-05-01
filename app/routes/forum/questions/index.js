ec0ac9e009409873629b0898ef740a9ef16eddac
Tim Hui <timboh56@gmail.com>
2016-05-01 03:35:55 -0700

    modify index

--- app/routes/forum/questions/index.js
+++ app/routes/forum/questions/index.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   scrollPagination: Ember.inject.service('scroll-pagination'),
+  queryParams: {
+    keywords: {
+      replace: true
+    }
+  },
+
   model: function(params) {
     if (params.query)
       return this.store.query('question', params);
