a2b4c8cd9fe0985ee28c8535c251d9f6cf58f1d9
Tim Hui <timboh56@gmail.com>
2016-05-22 16:37:49 -0700

    transitionTo login if not logged in

--- app/services/session.js
+++ app/services/session.js
@@ -2,47 +2,52 @@ import Ember from 'ember';
 import ENV from '../config/environment';
 
 export default Ember.Service.extend({
+  routing: Ember.inject.service('-routing'),
 
   getCredentials: function() {
     var self = this,
       username = Cookies.get('User'),
-      token = Cookies.get('Token'),
-      apiToken = Cookies.get('_mentormint-api_session');
+      token = Cookies.get('Token');
       console.log(username);
       console.log(token);
       var x = document.cookie;
 
       console.log(x);
 
+    if( username && token) {
+      return new Ember.RSVP.Promise(function(resolve, reject) {
+        $.ajax({
+          type: 'POST',
+          url: ENV.APP.LOGIN_PATH,
+          data: {
+           username: username,
+           token: token
+          },
 
-    return new Ember.RSVP.Promise(function(resolve, reject) {
-      $.ajax({
-        type: 'POST',
-        url: ENV.APP.LOGIN_PATH,
-        data: {
-         username: username || ENV.APP.TEST_USERNAME,
-        },
+          dataType: 'json',
 
-        dataType: 'json',
+          beforeSend(xhr) {
+            xhr.setRequestHeader('AUTHORIZATION', ENV.APIKEY);
+          },
 
-        beforeSend(xhr) {
-          xhr.setRequestHeader('AUTHORIZATION', ENV.APIKEY);
-        },
+          success: function(response) {
+            self.set('authToken', response.auth_token);
+            self.set('authUsername', response.username);
+            resolve(response.auth_token);
+            Ember.$.ajaxPrefilter(function( options, oriOptions, jqXHR ) {
+              jqXHR.setRequestHeader("AUTHORIZATION_TOKEN", response.auth_token);
+              jqXHR.setRequestHeader("AUTHORIZATION_USERNAME", response.username);
+            });
+          },
 
-        success: function(response) {
-          self.set('authToken', response.auth_token);
-          self.set('authUsername', response.username);
-          resolve(response.auth_token);
-          Ember.$.ajaxPrefilter(function( options, oriOptions, jqXHR ) {
-            jqXHR.setRequestHeader("AUTHORIZATION_TOKEN", response.auth_token);
-            jqXHR.setRequestHeader("AUTHORIZATION_USERNAME", response.username);
-          });
-        },
-
-        error: function(response) {
-          reject();
-        }
+          error: function(response) {
+            reject();
+          }
+        });
       });
-    });
+    } else {
+      console.log("You are not logged in");
+      this.get("routing").transitionTo("login");
+    }
   },
 });
